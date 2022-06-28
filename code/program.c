949879c2a6c1b1cf19fd79b1819a11b4ea5ba32c
Michael Gene Brockus <55331536+troglobyte-coder@users.noreply.github.com>
2022-06-28 15:35:50 -0700

    Update program.c

--- code/program.c
+++ code/program.c
@@ -33,9 +33,9 @@ const char *greet(void)
 int foundation(int argc, char **argv)
 {
     initscr();  /* Start curses mode */
-	printw("%s", greet()); /* Print Hello World */
-	refresh(); /* Print it on to the real screen */
-	getch();   /* Wait for user input */
+    printw("%s", greet()); /* Print Hello World */
+    refresh(); /* Print it on to the real screen */
+    getch();   /* Wait for user input */
     endwin();  /* End curses mode */
 
     return 0;
